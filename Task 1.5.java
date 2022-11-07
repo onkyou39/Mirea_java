df4c3422d2003819f21f9934ac5b03589afc6d65
onkyou39 <61075134+onkyou39@users.noreply.github.com>
2022-11-07 15:16:45 +0300

    Create Task 1.5.java

---
+++ Task 1.5.java
@@ -0,0 +1,11 @@
+//1st lab №5
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (String str : args) {
+            System.out.println("arg = " + str);
+        }
+
+    }
+}
